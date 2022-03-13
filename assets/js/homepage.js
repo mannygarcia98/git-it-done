9ec4726890018599bee85f5d9232523a3f19d159
Manny <mannysg88@gmail.com>
2022-03-13 15:29:01 -0500

    verified github endpoint

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,15 @@
-var getUserRepos = function (user) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+// var getUserRepos = function (user) {
+//   // format the github api url
+//   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-  // make a request to the url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  });
+//   // make a request to the url
+//   fetch(apiUrl).then(function (response) {
+//     response.json().then(function (data) {
+//       console.log(data);
+//     });
+//   });
+// };
+var getUserRepos = function () {
+  fetch("https://api.github.com/users/octocat/repos");
 };
-
 getUserRepos();
