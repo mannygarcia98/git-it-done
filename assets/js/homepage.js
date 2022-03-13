1188e677881bc68fdc79367628a6bfd4bbc53aec
Manny <mannysg88@gmail.com>
2022-03-13 15:43:11 -0500

    updated getUserRepos function to accept usernames as a parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,13 @@
-// var getUserRepos = function (user) {
-//   // format the github api url
-//   var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-//   // make a request to the url
-//   fetch(apiUrl).then(function (response) {
-//     response.json().then(function (data) {
-//       console.log(data);
-//     });
-//   });
-// };
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
+
 getUserRepos();
